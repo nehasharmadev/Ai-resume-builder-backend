88eb66b85f3072bc02981f6556d8c43250f3ba83
nehasharmadev <neha.sharma.dev2@gmail.com>
2025-06-17 16:11:29 +0530

    updated java version

--- src/main/java/com/backend/resume_ai_builder/services/ResumeServiceImpl.java
+++ src/main/java/com/backend/resume_ai_builder/services/ResumeServiceImpl.java
@@ -3,7 +3,6 @@ package com.backend.resume_ai_builder.services;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
